786439bee635eb97135f758b8968293293e45c8d
joao-hideki <joaoohideki12345@gmail.com>
2023-02-22 01:08:31 -0300

    configurando o Stitches

---
+++ src/styles/index.ts
@@ -0,0 +1,12 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  theme,
+  css,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  createTheme,
+} = createStitches()
