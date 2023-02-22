fa0fb867bee1bfca7a066a8644e57daba2cd3e26
joao-hideki <joaoohideki12345@gmail.com>
2023-02-22 01:25:51 -0300

    setando o tema e o estilo global

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -9,4 +9,18 @@ export const {
   keyframes,
   getCssText,
   createTheme,
-} = createStitches()
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      green500: '#00875f',
+      green300: '#00b37e'
+    }
+  }
+})
