ebafe30a5c700d50dabd9f61e305067c3cf79efb
joao-hideki <joaoohideki12345@gmail.com>
2023-02-24 00:23:41 -0300

    estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,9 +1,20 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
     const {query} = useRouter()
 
     return(
-        <div>Product</div>
+        <ProductContainer>
+          <ImageContainer>
+            imagem
+          </ImageContainer>
+          <ProductDetails>
+            <h1>Camiseta X</h1>
+            <span>R$ 79,90</span>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe neque laboriosam dolores inventore deleniti ipsa, vitae ratione accusamus odio tempore itaque voluptatibus velit quasi sint, animi a nesciunt, maiores consequatur.</p>
+            <button>Comprar agora</button>
+          </ProductDetails>
+        </ProductContainer>
     )
 }
