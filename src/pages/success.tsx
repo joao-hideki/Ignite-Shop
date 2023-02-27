da0acd85fe7ffe3f212299b768c879dbe8721a55
joao-hideki <joaoohideki12345@gmail.com>
2023-02-27 01:21:22 -0300

    estrutura da página Success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,5 +1,18 @@
+import { ImageContainer, SuccessContainer } from "@/styles/pages/success";
+import Link from "next/link";
+
 export default function Success() {
     return(
-        <div>Success</div>
+        <SuccessContainer>
+          <h1>Compra efetuada!</h1>
+          <ImageContainer></ImageContainer>
+          <p>
+            Uhuul <strong>User Name</strong>, sua <strong>Product Name</strong> já está a caminho da sua casa.
+          </p>
+
+          <Link href='/'>
+            Voltar ao catálogo
+          </Link>
+        </SuccessContainer>
     )
 }
